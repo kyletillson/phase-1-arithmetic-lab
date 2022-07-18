1a3282077987333d98de7ecb1237ad59755eadc4
Kyle Tillson <kyletillson004@gmail.com>
2022-07-18 16:21:17 -0600

    commit tree 57b48a95068b1532c1ad8673049d08f7158f4edc

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 62
+let num2 = 1
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 29
+let num4 = 5
+const mod = num3 % num4
+
+const max = 20
